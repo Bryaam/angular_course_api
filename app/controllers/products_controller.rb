7d1ad634af643b330155f934b1379650f298dc9b
Bryam Pacco <bryam.pacco@tektonlabs.com>
2018-04-08 18:02:43 -0500

    Products REST services

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,40 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+    json_response(@products)
+  end
+
+  def show
+    json_response(@product)
+  end
+
+
+  def create
+    @product = Product.create! products_params
+    json_response(@product, :created) 
+  end
+
+
+  def update
+    @product.update(products_params)
+    head :no_content
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def products_params
+    params.permit(:name, :description, :price, :image_url)
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "MAC", description: "Lorem Ipsum dolor at simet lorem ipsum das", price: "2500", image_url: "https://static-goldenfrog.netdna-ssl.com/images/vyprvpn/mac/banner_mac.png")
+Product.create(name: "Iphone", description:"Lorem Ipsum dolor at simet lorem ipsum das 3", price: "1500", image_url: "https://tienda.claro.com.ar/wcsstore/Claro/images/catalog/productos/646x1000/20302042.jpg")
+Product.create(name: "Audio Devise",description: "Lorem Ipsum dolor at simet lorem ipsum das 3", price: "500", image_url: "https://ae01.alicdn.com/kf/HTB1Ndnok26H8KJjSspmq6z2WXXaJ/Apple-Airpods-Original-Wireless-Bluetooth-In-ear-Earphone-Deeper-Bass-Tones-Connect-Siri-Powerful-Battery-For.jpg_640x640.jpg")
+Product.create(name: "Apple Watch",description: "Lorem Ipsum dolor at simet lorem ipsum das 4", price: "1000", image_url: "https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/4/2/42/alu/42-alu-silver-sport-white-s1-grid?wid=270&hei=275&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1512435128675")
